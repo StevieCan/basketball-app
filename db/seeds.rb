2ca77346f11be32e5c41f0e53426bc7372b8d724
Steve Han <shan61489@gmail.com>
2018-12-27 15:20:45 -0600

    initial data commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,3 +38,58 @@ Team.create!([
   {team: "Washington Wizards", points_allowed: 122, points_scored: 112 , avg_age: 26}
 
 ])
+
+
+Player.create!([
+  {name: "James Harden" , position: "Shooting Guard", age: 29, points: 32, team: "Houston Rockets"},
+  {name: "Kevin Durant", position: "Small Forward", age: 30, points: 29, team: "Golden State Warriors"},
+  {name: "Stephen Curry", position: "Point Guard", age: 30, points: 29, team: "Golden State Warriors"},
+  {name: "Anthony Davis", position: "Power Forward", age: 25, points: 28, team: "New Orleans Pelicans"},
+  {name: "Damian Lillard", position: "Point Guard", age: 28, points: 27, team: "Portland Trailblazers"},
+  {name: "Kawhi Leonard", position: "Small Forward", age: 27, points: 27, team: "Toronto Raptors"},
+  {name: "Joel Embiid", position: "Center", age: 24, points: 27, team: "Philadelphia 76ers"},
+  {name: "Giannis Antetokounmpo", position: "Small Forward", age: 24, points: 26, team: "Milwaukee Bucks"},
+  {name: "Paul George", position: "Small Forward", age: 28, points: 26, team: "Oklahoma Thunder"},
+  {name: "Devin Booker", position: "Shooting Guard", age: 22, points: 25, team: "Phoenix Suns"},
+  {name: "Blake Griffin", position: "Power Forward", age: 29, points: 25, team: "Detroit Pistons"},
+  {name: "Kemba Walker", position: "Point Guard", age: 28, points: 25, team: "Charlotte Hornets"},
+  {name: "Zach Lavine", position: "Shooting Guard", age: 23, points: 23, team: "Chicago Bulls"},
+  {name: "Bradley Beal", position: "Shooting Guard", age: 25, points: 24, team: "Washington Wizards"},
+  {name: "Demar Derozan", position: "Shooting Guard", age: 29, points: 23, team: "San Antonio Spurs"},
+  {name: "Kyrie Irving", position: "Point Guard", age: 26, points: 23, team: "Boston Celtics"},
+  {name: "Tobias Harris", position: "Small Forward", age: 26, points: 22, team: "Los Angeles Clippers"},
+  {name: "Klay Thompson", position: "Shooting Guard", age: 28, points: 21, team: "Golden State Warriors"},
+  {name: "CJ McCollum", position: "Shooting Guard", age: 27, points: 21, team: "Portland Trailblazers"},
+  {name: "Jrue Holiday", position: "Point Guard", age: 28, points: 21, team: "New Orleans Pelicans"},
+  {name: "Karl-Anthony Towns", position: "Center", age: 23, points: 21, team: "Minnesota Timberwolves"},
+  {name: "Tim Hardaway Jr", position: "Shooting Guard", age: 26, points: 21, team: "New York Knicks"},
+  {name: "John Wall", position: "Point Guard", age: 28, points: 21, team: "Washington Wizards"},
+  {name: "Mike Conley", position: "Point Guard", age: 31, points: 20, team: "Memphis Grizzlies"},
+  {name: "Russell Westbrook", position: "Point Guard", age: 30, points: 20, team: "Oklahoma City Thunder"},
+  {name: "Donovan Mitchell", position: "Point Guard", age: 22, points: 20, team: "Utah Jazz"},
+  {name: "Nikola Vucevic", position: "Power Forward", age: 28, points: 20, team: "Orlando Magic"},
+  {name: "Buddy Hield", position: "Shooting Guard", age: 26, points: 20, team: "Sacramento Kings"},
+  {name: "Victor Oladipo", position: "Point Guard", age: 26, points: 20, team: "Indiana Pacers"},
+  {name: "Jimmy Butler", position: "Shooting Guard", age: 29, points: 20, team: "Philadelphia 76ers"},
+  {name: "Danilo Gallinari", position: "Small Forward", age: 30, points: 20, team: "Los Angeles Clippers"},
+  {name: "Julius Randle", position: "Power Forward", age: 24, points: 19, team: "New Orleans Pelicans"},
+  {name: "Luka Doncic", position: "Point Guard", age: 19, points: 19, team: "Dallas Mavericks"},
+  {name: "Kevin Love", position: "Power Forward", age: 30, points: 19, team: "Cleveland Cavaliers"},
+  {name: "LaMarcus Aldridge", position: "Power Forward", age: 33, points: 19, team: "San Antonio Spurs"},
+  {name: "Derrick Rose", position: "Point Guard", age: 30, points: 19, team: "Minnesota Timberwolves"},
+  {name: "John Collins", position: "Center", age: 21, points: 19, team: "Atlanta Hawks"},
+  {name: "T.J. Warren", position: "Small Forward", age: 25, points: 19, team: "Phoenix Suns"},
+  {name: "Josh Richardson", position: "Small Forward", age: 25, points: 19, team: "Miami Heat"},
+  {name: "Caris LeVert", position: "Small Forward", age: 24, points: 18, team: "Brooklyn Nets"},
+  {name: "Harrison Barnes", position: "Small Forward", age: 26, points: 18, team: "Dallas Mavericks"},
+  {name: "Kyle Kuzma", position: "Power Forward", age: 23, points: 18, team: "Los Angeles Lakers"},
+  {name: "Spencer Dinwiddie", position: "Point Guard", age: 25, points: 18, team: "Brooklyn Nets"},
+  {name: "De'Aaron Fox", position: "Point Guard", age: 21, points: 18, team: "Sacramento Kings"},
+  {name: "JJ Redick", position: "Shooting Guard", age: 34, points: 18, team: "Philadelphia 76ers"},
+  {name: "Nikola Jokic", position: "Center", age: 23, points: 18, team: "Denver Nuggets"},
+  {name: "Andre Drummond", position: "Center", age: 25, points: 18, team: "Detroit Pistons"},
+  {name: "Lou Williams", position: "Shooting Guard", age: 32, points: 18, team: "Los Angeles Clippers"},
+  {name: "D'Angelo Russell", position: "Point Guard", age: 22, points: 18, team: "Brooklyn Nets"}
+
+
+])
